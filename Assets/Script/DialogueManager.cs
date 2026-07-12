988a77a71a48066a8d59c93536984f0773a719fc
伤心拖把 <yiwensimon.ding@gmail.com>
2026-07-12 18:21:55 +0800

    rain

--- Assets/Script/DialogueManager.cs
+++ Assets/Script/DialogueManager.cs
@@ -1,4 +1,6 @@
 using TMPro;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
@@ -6,6 +8,14 @@ public class DialogueManager : MonoBehaviour
     public TextMeshProUGUI SubtitleText;
 
 
+    [Serializable]
+    public class DialogueEntry // Single line of dialogue. Field names must match the JSON keys exactly
+    {
+        public string id;
+        public string character;
+        public string text;
+        public string nextId;
+    }
 
 
 }
