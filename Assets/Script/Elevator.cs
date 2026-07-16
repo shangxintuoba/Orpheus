726453a3792dee9b9a58fac461ededd604054f9f
伤心拖把 <yiwensimon.ding@gmail.com>
2026-07-16 17:15:05 +0800

    elevator initialize

---
+++ Assets/Script/Elevator.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject Door1;
+    public GameObject Door2;
+    public TextMeshProUGUI Level_text;
+    private int Level;
+    
+
+
+
+    private void OpenDoor()
+    {
+
+    }
+
+    private void CloseDoor()
+    {
+
+    }
+
+    private void UpdateLevelText()
+    {
+        
+    }
+
+
+    private void GoToFloor(int floor)
+    {
+
+    }
+
+
+}
